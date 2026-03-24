7d76b6fad6cce35d5917a97b6402709cb620a3d2
srijan393 <146333937+srijan393@users.noreply.github.com>
2026-03-24 12:50:48 +0000

    add api base path to post controller

--- src/main/java/com/socialhub/socialhub/controller/PostController.java
+++ src/main/java/com/socialhub/socialhub/controller/PostController.java
@@ -14,6 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class PostController {
 
     private final PostRepository postRepository;
